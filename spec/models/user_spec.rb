80363a0a5280c3110e413cbf3c283961806fa7be
pms <pms@efmac.org>
2017-02-24 18:21:21 +0800

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,14 @@ describe User do
   it{ should respond_to(:email)} 
   it{ should respond_to(:password_digest)}
   it{ should respond_to(:password)}
+  it{ should respond_to(:remember_token)}
   it{ should respond_to(:password_confirmation)}
   it{ should respond_to(:authenticate)}
+
+  describe "remember token" do
+    before{ @user.save }
+    its(:remember_token){ should_not be_blank}
+  end
   
   describe "name should be valid" do
     before {@user.name= ""}
